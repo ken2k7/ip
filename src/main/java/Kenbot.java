b9bdf9eb4f60890349a52e4cf0c6f1587fb6e518
Kenil <e1714042@u.nus.edu>
2026-08-26 01:11:07 +0800

    mark and unmark w/o class

--- src/main/java/Kenbot.java
+++ src/main/java/Kenbot.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Kenbot {
     static void main(String[] args) {
         String[] tasks = new String[100];
-        int taskcount = 0;
+        int taskCount = 0;
         String banner = " _  __          _           _        \n"
                 + "| |/ /___ _ __ | |__   ___ | |_      \n"
                 + "| ' // _ \\ '_ \\| '_ \\ / _ \\| __|     \n"
@@ -25,13 +25,14 @@ public class Kenbot {
 
       Scanner scn = new Scanner(System.in);
       String nxt = scn.nextLine();
-
       while(!nxt.isEmpty()) {
+          String[] words = nxt.split(" ");
           if (nxt.equals("list")) {
               //Print out the list
               System.out.println(line);
-              for (int i = 0; i < taskcount; i++) {
-                  System.out.println((i + 1) + ": " + tasks[i]);
+              System.out.println("Here are the tasks in your list:");
+              for (int i = 0; i < taskCount; i++) {
+                  System.out.println((i + 1) + "." + tasks[i]);
               }
               System.out.println(line);
           } else if (nxt.equals("bye")) {
@@ -40,10 +41,26 @@ public class Kenbot {
               System.out.println("Peace! See you soon!");
               System.out.println(line);
               break;
+          } else if (words[0].equals("mark")) {
+              int taskIndex = Integer.parseInt(words[1]) - 1;
+              tasks[taskIndex] = "[X]" + tasks[taskIndex].substring(3);
+
+              System.out.println(line);
+              System.out.println("Nice! I've marked this task as done:");
+              System.out.println("  " + tasks[taskIndex]);
+              System.out.println(line);
+          } else if (words[0].equals("unmark")) {
+              int taskIndex = Integer.parseInt(words[1]) - 1;
+              tasks[taskIndex] = "[ ]" + tasks[taskIndex].substring(3);
+
+              System.out.println(line);
+              System.out.println("OK, I've marked this task as not done yet:");
+              System.out.println("  " + tasks[taskIndex]);
+              System.out.println(line);
           } else {
               System.out.println(line);
-              tasks[taskcount] = nxt;
-              taskcount += 1;
+              tasks[taskCount] = "[ ] " + nxt;
+              taskCount += 1;
               System.out.println("Added: " + nxt);
               System.out.println(line);
           }
@@ -55,4 +72,3 @@ public class Kenbot {
       }
 
 }
-
