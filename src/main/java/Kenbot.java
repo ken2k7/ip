717d968ffc25c0ab36b37026d5c1cd0e753330cd
Kenil <e1714042@u.nus.edu>
2026-08-25 14:54:21 +0800

    Add-List

--- src/main/java/Kenbot.java
+++ src/main/java/Kenbot.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Kenbot {
     static void main(String[] args) {
+        String[] tasks = new String[100];
+        int taskcount = 0;
         String banner = " _  __          _           _        \n"
                 + "| |/ /___ _ __ | |__   ___ | |_      \n"
                 + "| ' // _ \\ '_ \\| '_ \\ / _ \\| __|     \n"
@@ -15,29 +17,42 @@ public class Kenbot {
 
       System.out.println(line);
       System.out.println(banner);
+
       System.out.println("Yo! I'm Kenbot");
       System.out.println("How may I help you today?");
+
       System.out.println(line + "\n");
+
       Scanner scn = new Scanner(System.in);
-      echo(scn);
+      String nxt = scn.nextLine();
+
+      while(!nxt.isEmpty()) {
+          if (nxt.equals("list")) {
+              //Print out the list
+              System.out.println(line);
+              for (int i = 0; i < taskcount; i++) {
+                  System.out.println((i + 1) + ": " + tasks[i]);
+              }
+              System.out.println(line);
+          } else if (nxt.equals("bye")) {
+              //exit the app
+              System.out.println(line);
+              System.out.println("Peace! See you soon!");
+              System.out.println(line);
+              break;
+          } else {
+              System.out.println(line);
+              tasks[taskcount] = nxt;
+              taskcount += 1;
+              System.out.println("Added: " + nxt);
+              System.out.println(line);
+          }
+          nxt = scn.nextLine();
+      }
+
       scn.close();
-    }
 
-    public static void echo(Scanner scn) {
-        String line = "____________________________________________________________";
-        String inp = scn.nextLine();
-
-        if(inp.equals("bye")) {
-            System.out.println(line);
-            System.out.println("Peace! See you soon!");
-            System.out.println(line);
-            return;
-        } else {
-            System.out.println(line);
-            System.out.println(inp);
-            System.out.println(line + "\n");
-            echo(scn);
-        }
-    }
+      }
 
 }
+
